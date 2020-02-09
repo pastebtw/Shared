e8f9b4221f74b872f708e8765b58f21e4bda7502
orion222 <56134554+orion222@users.noreply.github.com>
2020-02-08 19:17:29 -0500

    Add files via upload

---
+++ Practice/J4 Schedule Mr Singh.py
@@ -0,0 +1,42 @@
+import math
+
+tasks = int(input())
+lista = []
+listb = []
+listc = []
+
+for i in range(tasks):
+    priority, duration, name = input().split()
+    name = list(name)
+    name.remove('"')
+    name.remove('"')
+    name = ''.join(name)
+    lista.append(int(priority))
+    listb.append(int(duration))
+    listc.append(name)
+
+important = lista.index(max(lista))
+final = listc[important]
+
+minutes = sum(listb) * 3 % 60
+hours = (sum(listb) * 3/ 60)
+hours = math.floor(hours)
+minutes = int(minutes)
+time = hours + minutes
+
+
+print(final)
+if hours < 13:
+    print('0' + str(hours) + str(minutes))
+else:
+    print(hours + minutes)
+
+
+
+
+
+
+
+
+
+
