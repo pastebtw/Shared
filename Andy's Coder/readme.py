f7663a15505190e93d56fa77fab824cf30caf37c
orion222 <56134554+orion222@users.noreply.github.com>
2020-02-21 16:52:22 -0500

    Create readme.py

---
+++ Andy's Coder/readme.py
@@ -0,0 +1,13 @@
+import time
+
+time = time.asctime()
+
+
+# Who is legal?
+
+andyvictim1 = {'name': 'Scott', 'age': '14', 'ethnicity': 'White'}
+andyvictim2 = {'name': 'Aidan', 'age': '14', 'ethnicity': 'Corona'}
+andyvictim3 = {'name': 'Cyron', 'age': '14', 'ethnicity': 'Filipino'}
+
+print("It is", time)
+print("None of them are legal. Sorry")
